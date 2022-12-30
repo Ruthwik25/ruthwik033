21035307939da07887e4f6b253f114f433b29819
RUTHWIK <RUTHWIK@DESKTOP-27NSV1D>
2022-12-30 14:30:26 +0530

    actual and formal parameters

---
+++ actualformalp/src/actualformalp/driver.java
@@ -0,0 +1,10 @@
+package actualformalp;
+
+public class driver {
+public static void main(String[]args) {
+	objectcls o;
+	o=new objectcls();
+	int b=o.display(2, 3);
+	System.out.println("b="+b);
+}
+}
---
+++ actualformalp/src/actualformalp/objectcls.java
@@ -0,0 +1,8 @@
+package actualformalp;
+
+public class objectcls {
+	int display(int x,int y) {
+	int a;
+	return  a=x+y;
+}
+}
